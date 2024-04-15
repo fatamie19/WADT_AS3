207fcd4036f3e619de8e11d4ea7af4ca1636914d
fatamie19 <22ftt1507@student.pb.edu.bn>
2024-04-16 00:48:21 +0800

    Added Hobbies, Goals and Responsiveness

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 </script>
 
-<template>
+<template class="bg-">
   <div class="flex flex-col items-center space-y-4">
-    <img class="w-72 md:w-80 lg:w-96 rounded" src="./assets/KCW.jpg">
+    <img class="w-72 md:w-80 lg:w-96 rounded border-4 border-white" src="./assets/KCW.jpg">
   </div>
   <br>
-  <div class="flex flex-col items-center space-y-2 bg-white text-black hover:bg-black hover:text-white px-10 py-2 rounded">
+  <div class="flex flex-col items-center space-y-2 bg-white text-black hover:bg-black hover:text-white py-2 rounded">
     <p class="font-serif text-3xl md:text-4xl lg:text-5xl">About Me</p>
     <p class="text-base md:text-lg lg:text-xl">Name: Fatin Amierah</p>
     <p class="text-base md:text-lg lg:text-xl">Age: 19 Years old</p>
@@ -15,8 +15,11 @@
   </div>
   <br>
 
+  <div class="grid grid-cols-2 gap-2 text-black">
+    <div class="bg-white rounded py-2 px-2 hover:bg-black hover:text-white sm:w-48 md:w-60 lg:w-96">Hobbies: <br> In my freetime, I immerse myself in YouTube, where I indulge in educational content and entertaining vlogs.</div>
+    <div class="bg-white rounded py-2 px-2 hover:bg-black hover:text-white sm:w-48 md:w-60 lg:w-96">Goals: <br> My goal is to secure a rewarding career that offers financial stability and enables me to make a positive impact in my field. </div>
+  </div>
 </template>
 
 <style scoped>
-
 </style>
