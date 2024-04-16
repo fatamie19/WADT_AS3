3cfe54ac8dd483447d278c5a07481d6a9bd03817
fatamie19 <22ftt1507@student.pb.edu.bn>
2024-04-16 21:04:46 +0800

    Trying to push

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
   <br>
   <div class="flex flex-col items-center space-y-2 bg-white text-black hover:bg-black hover:text-white py-2 rounded">
     <p class="font-serif text-3xl md:text-4xl lg:text-5xl">About Me</p>
-    <p class="text-base md:text-lg lg:text-xl">Name: Fatin Amierah</p>
+    <p class="text-base md:text-lg lg:text-xl">Name: Fatin Amierah Hj Ramli</p>
     <p class="text-base md:text-lg lg:text-xl">Age: 19 Years old</p>
     <p class="text-base md:text-lg lg:text-xl">Course: Web Development</p>
     <p class="text-base md:text-lg lg:text-xl">College: Politeknik Brunei</p>
